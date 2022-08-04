753cd4ac1d590b6741a36021b1972fc92f994a1c
mcossutta <mcossutta@gmail.com>
2022-08-04 21:50:36 +0200

    comment migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # le fichier config.py contient les variables de configuration
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 
 from app import routes, models
